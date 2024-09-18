8fb8047d509bb100645ad8973e3c51593843cf59
Nora Soraa <notjsoraa@icloud.com>
2024-09-18 12:48:03 +0200

    fikset oppsett

---
+++ oblig2/src/oppgave2/Hamburger.java
@@ -0,0 +1,18 @@
+package oppgave2;
+
+public class Hamburger {
+	private final int id;
+	
+	public Hamburger(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "◖ " + id + " ◗";
+	}
+}
---
+++ oblig2/src/oppgave3/HamburgerBrett.java
@@ -0,0 +1,26 @@
+package oppgave3;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class HamburgerBrett {
+	private final BlockingQueue<Hamburger> brett;
+    private int hamburgerTeller = 0;
+	
+	public HamburgerBrett(int kapasitet) {
+		this.brett = new ArrayBlockingQueue<>(kapasitet);
+	}
+	
+	public void leggTilHamburger(String kokkNavn) throws InterruptedException {
+		hamburgerTeller++;
+        Hamburger hamburger = new Hamburger(hamburgerTeller);
+        brett.put(hamburger);  // Blokkerer hvis brettet er fullt
+		System.out.println(kokkNavn + " (kokk) legger på hamburger " + hamburger + ". Brett: " + brett);
+	}
+	
+	public Hamburger fjernHamburger(String servitorNavn) throws InterruptedException {
+		 Hamburger hamburger = brett.take();  // Blokkerer hvis brettet er tomt
+	        System.out.println(servitorNavn + " (servitør) tar av hamburger " + hamburger + ". Brett: " + brett);
+	        return hamburger;
+	}
+}
